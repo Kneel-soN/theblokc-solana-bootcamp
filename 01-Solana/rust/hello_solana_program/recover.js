79afd2fe6e70e13da05917c614212fc6402dd40a
Kneel-soN <125803337+Kneel-soN@users.noreply.github.com>
2023-07-10 05:27:13 +0800

    Delete 01-Solana/rust directory

--- 01-Solana/rust/hello_solana_program/recover.js
+++
@@ -1,6 +0,0 @@
-// recover.js
-const bs58 = require('bs58');
-const fs = require('fs');
-b = bs58.decode('2749aGqtrsDEViPc67oQrfEH53DfQpEdV8Y2HkCJa5dxV3kDg7Y7F79rm4r6ymDKNjn7cFt5bdC9RKSmcYCKYwzj');
-j = new Uint8Array(b.buffer, b.byteOffset, b.byteLength / Uint8Array.BYTES_PER_ELEMENT);
-fs.writeFileSync('key.json', `[${j}]`); 
