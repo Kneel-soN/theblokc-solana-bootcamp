c0267415567a4b9f4de0356337b66889bc8b6343
Kneel-soN <125803337+Kneel-soN@users.noreply.github.com>
2023-07-20 05:20:17 +0800

    Update frontpage.js

--- nextjs-anchor-dapp/app/frontpage.js
+++ nextjs-anchor-dapp/app/frontpage.js
@@ -1,54 +1,42 @@
-'use client'
-import './App.css';
-import Image from "next/image"
-import viper from './assets/viper.png'
-import { useState} from 'react'
-
-import idl from './user_input.json'
-import { Connection, PublicKey, clusterApiUrl  } from '@solana/web3.js';
-import { Program, AnchorProvider, web3, } from '@project-serum/anchor';
+import React, { useState } from 'react';
+import Image from 'next/image';
+import viper from './assets/viper.png';
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import { Program, AnchorProvider, web3 } from '@project-serum/anchor';
 import * as Web3 from '@solana/web3.js';
-const {SystemProgram,Keypair} = web3;
 
-const programID = new PublicKey('UhH9QdWTjFedFNtasBBaxwP7A52JPipkVMqBZuLR6QB')
+const { SystemProgram, Keypair } = web3;
+
+const programID = new PublicKey('UhH9QdWTjFedFNtasBBaxwP7A52JPipkVMqBZuLR6QB');
 const opts = {
-  preflightCommitment:"processed",
-}
+  preflightCommitment: 'processed',
+};
 
 
 //const network = "http://127.0.0.1:8899";  // for localnet
 //const network = clusterApiUrl("devnet") // for devnet
-const network = clusterApiUrl("testnet") // for testnet
-
+const network = clusterApiUrl('testnet'); // for testnet
 
 const new_account = Keypair.generate();
-console.log(new_account)
+console.log(new_account);
 
 function Frontpage() {
-  
-  const [userInput, setUserInput] = useState("");
-  const [walletaddress, setWalletAddress] = useState("");
-  const [Tx, setTx] = useState("");
+  const [userInput, setUserInput] = useState('');
+  const [walletaddress, setWalletAddress] = useState('');
+  const [Tx, setTx] = useState('');
   const [txSig, setTxSig] = useState('');
   const [transactionDetails, setTransactionDetails] = useState(null);
   const [txDone, setTxDone] = useState(false);
 
-
-
   const getProvider = () => {
     const connection = new Connection(network, opts.preflightCommitment);
-    const provider = new AnchorProvider(
-      connection,
-      window.solana,
-      opts.preflightCommitment
-    );
+    const provider = new AnchorProvider(connection, window.solana, opts.preflightCommitment);
     return provider;
   };
 
-
   const connectWallet = async () => {
     if (!window.solana) {
-      alert("Solana wallet not found. Please install Sollet or Phantom extension.");
+      alert('Solana wallet not found. Please install Sollet or Phantom extension.');
       return;
     }
 
@@ -58,106 +46,89 @@ function Frontpage() {
       const walletAddress = provider.wallet.publicKey.toString();
       setWalletAddress(walletAddress);
     } catch (err) {
-      console.error("Error connecting wallet:", err);
+      console.error('Error connecting wallet:', err);
     }
   };
 
+  async function input() {
+    const dataAcc = new_account;
+    console.log(dataAcc);
+    const provider = getProvider();
+    const program = new Program(idl, programID, provider);
 
+    try {
+      const txSignature = await program.rpc.initialize(userInput, {
+        accounts: {
+          newAccount: new_account.publicKey,
+          signer: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [new_account],
+      });
 
+      const confirmation = await provider.connection.confirmTransaction(txSignature, 'confirmed');
+      console.log('Transaction Confirmation:', confirmation);
 
-async function input() {
-  const dataAcc = new_account;
-  console.log(dataAcc);
-  const provider = getProvider();
-  const program = new Program(idl, programID, provider);
+      console.log('Transaction Signature:', txSignature);
 
-  try {
-
-    const txSignature = await program.rpc.initialize(userInput, {
-      accounts: {
-        newAccount: new_account.publicKey,
-        signer: provider.wallet.publicKey,
-        systemProgram: SystemProgram.programId,
-      },
-      signers: [new_account],
-    });
-
-    const confirmation = await provider.connection.confirmTransaction(txSignature, 'confirmed');
-    console.log('Transaction Confirmation:', confirmation);
-
-
-    console.log('Transaction Signature:', txSignature);
-
-    setTx(txSignature);
-     setTxDone(true)
-    const account = await program.account.newAccount.fetch(new_account.publicKey);
-    console.log('Output:', account);
-  } catch (err) {
-    console.error("Transaction Error:", err);
-  }
-}
-
-
-
-  
-const findTxRes = async () => {
-  try {
-    const conn = new Web3.Connection('https://api.testnet.solana.com');
-    const fetchedTransaction = await conn.getConfirmedTransaction(txSig);
-
-    if (fetchedTransaction) {
-      setTransactionDetails(fetchedTransaction);
-    } else {
-      console.log('Transaction not found.');
+      setTx(txSignature);
+      setTxDone(true);
+      const account = await program.account.newAccount.fetch(new_account.publicKey);
+      console.log('Output:', account);
+    } catch (err) {
+      console.error('Transaction Error:', err);
     }
-  } catch (error) {
-    console.error('Error fetching transaction:', error);
   }
-};
 
+  const findTxRes = async () => {
+    try {
+      const conn = new Web3.Connection('https://api.testnet.solana.com');
+      const fetchedTransaction = await conn.getConfirmedTransaction(txSig);
 
-
-
-
+      if (fetchedTransaction) {
+        setTransactionDetails(fetchedTransaction);
+      } else {
+        console.log('Transaction not found.');
+      }
+    } catch (error) {
+      console.error('Error fetching transaction:', error);
+    }
+  };
 
   return (
-
-    
     <div className="App">
       <header className="App-header">
-      <p style={{ fontSize: '16px', color: 'violet' }}>Address: {walletaddress}</p>
-    <Image src={viper} alt="Logo" height={100}width={360}className="App-logo" />
+        <p style={{ fontSize: '16px', color: 'violet' }}>Address: {walletaddress}</p>
+        <Image src={viper} alt="Logo" height={100} width={360} className="App-logo" />
         <button onClick={connectWallet}>Connect Wallet</button>
-<p>Type Something in here</p>
-
-
-  <input value={userInput} onChange={(e) => setUserInput(e.target.value)} />
-<button onClick={input}>Submit</button>
-<p style={{ fontSize: '16px', color: 'violet' }}>Signature: {Tx}</p>
-{txDone && (
-  <div>
-          <p style={{ fontSize: 'smaller' }}>
-            Transaction confirmed!{' '}
-            <a
-              href={`https://explorer.solana.com/tx/${Tx}?cluster=testnet`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View on Solana Explorer
-            </a>
-          </p>
-          <p style={{ fontSize: '16px', color: 'yellow' }}>Please wait for a bit before trying to fetch</p>
+        <p>Type Something in here</p>
+        <input value={userInput} onChange={(e) => setUserInput(e.target.value)} />
+        <button onClick={input}>Submit</button>
+        <p style={{ fontSize: '16px', color: 'violet' }}>Signature: {Tx}</p>
+        {txDone && (
+          <div>
+            <p style={{ fontSize: 'smaller' }}>
+              Transaction confirmed!{' '}
+              <a
+                href={`https://explorer.solana.com/tx/${Tx}?cluster=testnet`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View on Solana Explorer
+              </a>
+            </p>
+            <p style={{ fontSize: '16px', color: 'yellow' }}>Please wait for a bit before trying to fetch</p>
           </div>
-        )}  
-<input
-            type="text"
-            value={txSig}
-            onChange={(event) => setTxSig(event.target.value)}
-            placeholder="Enter Transaction Signature"
-          />
-          <button onClick={findTxRes}>Fetch Transaction</button>
-   
-          {transactionDetails ? (
+        )}
+        <input
+          type="text"
+          value={txSig}
+          onChange={(event) => setTxSig(event.target.value)}
+          placeholder="Enter Transaction Signature"
+        />
+        <button onClick={findTxRes}>Fetch Transaction</button>
+
+        {transactionDetails ? (
           <div>
             <h4>Transaction Details</h4>
             {transactionDetails.transaction.instructions.length >= 2 ? (
@@ -175,16 +146,15 @@ const findTxRes = async () => {
         ) : (
           <p></p>
         )}
-  <div>
-<p style={{ fontSize: '16px' }>Note: Phantom Wallet may block your access to this dapp, if you are unsure of account's safety, you can check my repo </p>
-     <a
-              href={`https://github.com/Kneel-soN/theblokc-solana-bootcamp/tree/main/nextjs-anchor-dapp`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-Here     
-            </a>
-</div>
+        <div>
+          <p style={{ fontSize: '16px' }}>
+            Note: Phantom Wallet may block your access to this dapp, if you are unsure of account's safety, you can
+            check my repo{' '}
+          </p>
+          <a href="https://github.com/Kneel-soN/theblokc-solana-bootcamp/tree/main/nextjs-anchor-dapp" target="_blank" rel="noopener noreferrer">
+            Here
+          </a>
+        </div>
       </header>
     </div>
   );
