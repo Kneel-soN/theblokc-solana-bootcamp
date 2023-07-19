c50b4169fe3f7ef491068e0c495433d68549b9fe
Kneel-soN <125803337+Kneel-soN@users.noreply.github.com>
2023-07-20 05:27:23 +0800

    Update frontpage.js

--- nextjs-anchor-dapp/app/frontpage.js
+++ nextjs-anchor-dapp/app/frontpage.js
@@ -176,7 +176,7 @@ const findTxRes = async () => {
           <p></p>
         )}
   <div>
-<p>Note: Phantom Wallet may block your access to this dapp, if you are unsure of account's safety, you can check my repo </p>
+<p style={{ fontSize: 'smaller' }>Note: Phantom Wallet may block your access to this dapp, if you are unsure of your account's safety, you can check my repo </p>
      <a
               href={`https://github.com/Kneel-soN/theblokc-solana-bootcamp/tree/main/nextjs-anchor-dapp`}
               target="_blank"
